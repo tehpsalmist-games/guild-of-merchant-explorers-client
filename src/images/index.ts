d168dcdd05ddaf152c2c35e1bf686c5e8ef475e3
tehpsalmist <bstewardcodes@gmail.com>
2024-04-19 16:55:19 -0400

    moar images

--- src/images/index.ts
+++ src/images/index.ts
@@ -1,3 +1,4 @@
+// pieces
 export const blockImage = new URL('/src/images/pieces/block-blank.png', import.meta.url)
 export const villageImage = new URL('/src/images/pieces/village-blank.png', import.meta.url)
 export const treasureChestImage = new URL('/src/images/pieces/treasure-chest.png', import.meta.url)
@@ -6,11 +7,13 @@ export const towerImage = new URL('/src/images/pieces/tower.png', import.meta.ur
 export const coinImage = new URL('/src/images/pieces/coin.png', import.meta.url)
 export const tradePostCoverImage = new URL('/src/images/pieces/crystal.png', import.meta.url)
 
+// boards
 export const aghonBoard = new URL('/src/images/boards/aghon.png', import.meta.url)
 export const aveniaBoard = new URL('/src/images/boards/avenia.png', import.meta.url)
 export const kazanBoard = new URL('/src/images/boards/kazan.png', import.meta.url)
 export const cnidariaBoard = new URL('/src/images/boards/cnidaria.png', import.meta.url)
 
+// explorer cards
 export const exploreSand = new URL('/src/images/explorer-cards/sand.png', import.meta.url)
 export const exploreGrass = new URL('/src/images/explorer-cards/grass.png', import.meta.url)
 export const exploreMountain = new URL('/src/images/explorer-cards/mountain.png', import.meta.url)
@@ -21,6 +24,7 @@ export const exploreEra2 = new URL('/src/images/explorer-cards/era-2.png', impor
 export const exploreEra3 = new URL('/src/images/explorer-cards/era-3.png', import.meta.url)
 export const exploreEraAny = new URL('/src/images/explorer-cards/any-era.png', import.meta.url)
 
+// treasure cards
 export const grassVillageBonus = new URL('/src/images/treasure-cards/grass-village-bonus.png', import.meta.url)
 export const jarMultiplier = new URL('/src/images/treasure-cards/jar-multiplier.png', import.meta.url)
 export const landVillageHalfBonus = new URL('/src/images/treasure-cards/land-village-half-bonus.png', import.meta.url)
@@ -29,3 +33,142 @@ export const placeBlock = new URL('/src/images/treasure-cards/place-block.png', 
 export const sandVillageBonus = new URL('/src/images/treasure-cards/sand-village-bonus.png', import.meta.url)
 export const towerBonus = new URL('/src/images/treasure-cards/tower-bonus.png', import.meta.url)
 export const twoCoins = new URL('/src/images/treasure-cards/two-coins.png', import.meta.url)
+
+// power cards
+export const powerCardconnectedlistgrasswater5 = new URL(
+  '/src/images/power-cards/power-card-connected-list-grass-water-5.png',
+  import.meta.url,
+)
+export const powerCardconnectedlistmountainwater4 = new URL(
+  '/src/images/power-cards/power-card-connected-list-mountain-water-4.png',
+  import.meta.url,
+)
+export const powerCardconnectedlistsandwater5 = new URL(
+  '/src/images/power-cards/power-card-connected-list-sand-water-5.png',
+  import.meta.url,
+)
+export const powerCardgrass3mountain2 = new URL(
+  '/src/images/power-cards/power-card-grass-3-mountain-2.png',
+  import.meta.url,
+)
+export const powerCardgrass4water1 = new URL('/src/images/power-cards/power-card-grass-4-water-1.png', import.meta.url)
+export const powerCardinfinitelineland = new URL(
+  '/src/images/power-cards/power-card-infinite-line-land.png',
+  import.meta.url,
+)
+export const powerCardinfinitelinewater = new URL(
+  '/src/images/power-cards/power-card-infinite-line-water.png',
+  import.meta.url,
+)
+export const powerCardlistcoinbonus2mountain3 = new URL(
+  '/src/images/power-cards/power-card-list-coin-bonus-2-mountain-3.png',
+  import.meta.url,
+)
+export const powerCardlistcoinbonus3grass4 = new URL(
+  '/src/images/power-cards/power-card-list-coin-bonus-3-grass-4.png',
+  import.meta.url,
+)
+export const powerCardlistcoinbonus3sand4 = new URL(
+  '/src/images/power-cards/power-card-list-coin-bonus-3-sand-4.png',
+  import.meta.url,
+)
+export const powerCardmountain3water1 = new URL(
+  '/src/images/power-cards/power-card-mountain-3-water-1.png',
+  import.meta.url,
+)
+export const powerCardregion4 = new URL('/src/images/power-cards/power-card-region-4.png', import.meta.url)
+export const powerCardsand1grass1mountain1water2 = new URL(
+  '/src/images/power-cards/power-card-sand-1-grass-1-mountain-1-water-2.png',
+  import.meta.url,
+)
+export const powerCardsand3mountain2 = new URL(
+  '/src/images/power-cards/power-card-sand-3-mountain-2.png',
+  import.meta.url,
+)
+export const powerCardsand4water1 = new URL('/src/images/power-cards/power-card-sand-4-water-1.png', import.meta.url)
+export const powerCardsurroundinggrass = new URL(
+  '/src/images/power-cards/power-card-surrounding-grass.png',
+  import.meta.url,
+)
+export const powerCardsurroundingmountain = new URL(
+  '/src/images/power-cards/power-card-surrounding-mountain.png',
+  import.meta.url,
+)
+export const powerCardsurroundingsand = new URL(
+  '/src/images/power-cards/power-card-surrounding-sand.png',
+  import.meta.url,
+)
+export const powerCardsurroundingwater = new URL(
+  '/src/images/power-cards/power-card-surrounding-water.png',
+  import.meta.url,
+)
+export const powerCardtravellingfromgrass = new URL(
+  '/src/images/power-cards/power-card-travelling-from-grass.png',
+  import.meta.url,
+)
+export const powerCardtravellingfrommountain = new URL(
+  '/src/images/power-cards/power-card-travelling-from-mountain.png',
+  import.meta.url,
+)
+export const powerCardtravellingfromsand = new URL(
+  '/src/images/power-cards/power-card-travelling-from-sand.png',
+  import.meta.url,
+)
+export const powerCardtravellingfromvillage2 = new URL(
+  '/src/images/power-cards/power-card-travelling-from-village-2.png',
+  import.meta.url,
+)
+export const powerCardtravellingfromwater = new URL(
+  '/src/images/power-cards/power-card-travelling-from-water.png',
+  import.meta.url,
+)
+export const powerCardwater4cities = new URL('/src/images/power-cards/power-card-water-4-cities.png', import.meta.url)
+export const powerCardwater4coin2 = new URL('/src/images/power-cards/power-card-water-4-coin-2.png', import.meta.url)
+export const powerCardwater4doubletreasure = new URL(
+  '/src/images/power-cards/power-card-water-4-double-treasure.png',
+  import.meta.url,
+)
+export const powerCardwater5 = new URL('/src/images/power-cards/power-card-water-5.png', import.meta.url)
+
+// objective cards
+export const aghonObjective1 = new URL('/src/images/objectives/aghon/objective 1.png', import.meta.url)
+export const aghonObjective2 = new URL('/src/images/objectives/aghon/objective 2.png', import.meta.url)
+export const aghonObjective3 = new URL('/src/images/objectives/aghon/objective 3.png', import.meta.url)
+export const aghonObjective4 = new URL('/src/images/objectives/aghon/objective 4.png', import.meta.url)
+export const aghonObjective5 = new URL('/src/images/objectives/aghon/objective 5.png', import.meta.url)
+export const aghonObjective6 = new URL('/src/images/objectives/aghon/objective 6.png', import.meta.url)
+
+export const aveniaObjective1 = new URL('/src/images/objectives/avenia/objective 1.png', import.meta.url)
+export const aveniaObjective2 = new URL('/src/images/objectives/avenia/objective 2.png', import.meta.url)
+export const aveniaObjective3 = new URL('/src/images/objectives/avenia/objective 3.png', import.meta.url)
+export const aveniaObjective4 = new URL('/src/images/objectives/avenia/objective 4.png', import.meta.url)
+export const aveniaObjective5 = new URL('/src/images/objectives/avenia/objective 5.png', import.meta.url)
+export const aveniaObjective6 = new URL('/src/images/objectives/avenia/objective 6.png', import.meta.url)
+
+export const kazanObjective1 = new URL('/src/images/objectives/kazan/objective 1.png', import.meta.url)
+export const kazanObjective2 = new URL('/src/images/objectives/kazan/objective 2.png', import.meta.url)
+export const kazanObjective3 = new URL('/src/images/objectives/kazan/objective 3.png', import.meta.url)
+export const kazanObjective4 = new URL('/src/images/objectives/kazan/objective 4.png', import.meta.url)
+export const kazanObjective5 = new URL('/src/images/objectives/kazan/objective 5.png', import.meta.url)
+export const kazanObjective6 = new URL('/src/images/objectives/kazan/objective 6.png', import.meta.url)
+
+export const cnidariaObjective1 = new URL('/src/images/objectives/cnidaria/objective 1.png', import.meta.url)
+export const cnidariaObjective2 = new URL('/src/images/objectives/cnidaria/objective 2.png', import.meta.url)
+export const cnidariaObjective3 = new URL('/src/images/objectives/cnidaria/objective 3.png', import.meta.url)
+export const cnidariaObjective4 = new URL('/src/images/objectives/cnidaria/objective 4.png', import.meta.url)
+export const cnidariaObjective5 = new URL('/src/images/objectives/cnidaria/objective 5.png', import.meta.url)
+export const cnidariaObjective6 = new URL('/src/images/objectives/cnidaria/objective 6.png', import.meta.url)
+
+export const xawskilObjective1 = new URL('/src/images/objectives/xawskil/objective 1.png', import.meta.url)
+export const xawskilObjective2 = new URL('/src/images/objectives/xawskil/objective 2.png', import.meta.url)
+export const xawskilObjective3 = new URL('/src/images/objectives/xawskil/objective 3.png', import.meta.url)
+export const xawskilObjective4 = new URL('/src/images/objectives/xawskil/objective 4.png', import.meta.url)
+export const xawskilObjective5 = new URL('/src/images/objectives/xawskil/objective 5.png', import.meta.url)
+export const xawskilObjective6 = new URL('/src/images/objectives/xawskil/objective 6.png', import.meta.url)
+
+export const northProyliaObjective1 = new URL('/src/images/objectives/north-proylia/objective 1.png', import.meta.url)
+export const northProyliaObjective2 = new URL('/src/images/objectives/north-proylia/objective 2.png', import.meta.url)
+export const northProyliaObjective3 = new URL('/src/images/objectives/north-proylia/objective 3.png', import.meta.url)
+export const northProyliaObjective4 = new URL('/src/images/objectives/north-proylia/objective 4.png', import.meta.url)
+export const northProyliaObjective5 = new URL('/src/images/objectives/north-proylia/objective 5.png', import.meta.url)
+export const northProyliaObjective6 = new URL('/src/images/objectives/north-proylia/objective 6.png', import.meta.url)
