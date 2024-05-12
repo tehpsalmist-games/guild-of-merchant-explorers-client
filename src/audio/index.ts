9b1af39a4ed367df7511d7b59109c22087a1bfcd
Dylan Raley <104690026+dylanraleyisme@users.noreply.github.com>
2024-05-12 19:15:54 -0400

    Added some audio to play around with

---
+++ src/audio/index.ts
@@ -0,0 +1,11 @@
+export const placeBlockSFX = new URL('/src/audio/place-block.wav', import.meta.url)
+export const villageSFX = new URL('/src/audio/village.wav', import.meta.url)
+
+export const coin1SFX = new URL('/src/audio/coin-1.wav', import.meta.url)
+export const coin2SFX = new URL('/src/audio/coin-2.wav', import.meta.url)
+
+export const cardFlipSFX = new URL('/src/audio/card-flip.wav', import.meta.url)
+
+export const towerSFX = new URL('/src/audio/tower.wav', import.meta.url)
+export const treasureSFX = new URL('/src/audio/treasure.wav', import.meta.url)
+export const crystalSFX = new URL('/src/audio/crystal.wav', import.meta.url)
